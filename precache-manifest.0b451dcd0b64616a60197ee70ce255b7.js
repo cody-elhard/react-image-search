55b2227c3036cacb9948a20215ecc31da41e52fc
Cody Elhard <cody.elhard@ndsu.edu>
2018-12-13 21:14:32 -0600

    Updates

---
+++ precache-manifest.0b451dcd0b64616a60197ee70ce255b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cabfdc7f712a65cced8",
+    "url": "/react-image-search/static/js/runtime~main.7cabfdc7.js"
+  },
+  {
+    "revision": "18ff7bfe8b8f03648de7",
+    "url": "/react-image-search/static/js/main.18ff7bfe.chunk.js"
+  },
+  {
+    "revision": "dda695a3091ade7f9d9f",
+    "url": "/react-image-search/static/js/1.dda695a3.chunk.js"
+  },
+  {
+    "revision": "18ff7bfe8b8f03648de7",
+    "url": "/react-image-search/static/css/main.00d27937.chunk.css"
+  },
+  {
+    "revision": "492cb35fce9202d5972668adde56fd33",
+    "url": "/react-image-search/index.html"
+  }
+];
